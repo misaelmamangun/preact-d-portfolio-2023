33936b77427bc9b19c9c7197ac19672de8abbcb8
Misael Paul Mamangun <misaelpaulmamangun@gmail.com>
2023-07-31 17:21:15 +0800

    update desc

--- src/layouts/Hero/index.jsx
+++ src/layouts/Hero/index.jsx
@@ -1,29 +1,29 @@
-import logo from './../../assets/settings/logo.png'
+import logo from "./../../assets/settings/logo.png";
 
 const Hero = () => {
   return (
     <>
-      <div className='md:h-screen'>
-        <div className='container mx-auto px-4 h-full'>
-          <div className='md:grid items-center grid-cols-2 h-full h-full gap-4'>
+      <div className="md:h-screen">
+        <div className="container mx-auto px-4 h-full">
+          <div className="md:grid items-center grid-cols-2 h-full h-full gap-4">
             <div>
-              <h1 className='md:text-7xl md:mt-0 text-4xl mt-4 uppercase text-light-400 border-b-4 mb-4'>
+              <h1 className="md:text-7xl md:mt-0 text-4xl mt-4 uppercase text-light-400 border-b-4 mb-4">
                 Misael Paul Mamangun
               </h1>
-              <p className='text-light-400 font-bold'>
-                I'm a freelance web developer from the Philippines who is
+              <p className="text-light-400 font-bold">
+                I'm a frontend web developer from the Philippines who is
                 passionate about building simple, modern, and responsive
                 website.
               </p>
             </div>
             <div>
-              <img src={logo} alt='logo' />
+              <img src={logo} alt="logo" />
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
